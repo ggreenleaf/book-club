fb12f4c72bc968695f0526459352c4aeb801dc45
ggreenleaf <geoffrey.greenleaf@gmail.com>
2018-07-02 21:30:27 -0500

    added core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { throwIfAlreadyLoaded } from './module-import-guard';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [],
+  declarations: [],
+  providers: []
+})
+export class CoreModule { 
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule')
+  }
+}
+
